7e65a03d0a458fcb3638da95c0a1125039c8af11
Neekon <neekon91@gmail.com>
2017-01-08 20:01:22 -0800

    extend

--- src/underbar.js
+++ src/underbar.js
@@ -135,7 +135,11 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = (obj) => {
+  _.extend = function(obj) {
+    _.each(arguments, (nextObj) =>
+      _.each(nextObj, (value, key) =>
+        obj[key] = value));
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
