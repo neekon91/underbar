c55e65416584aa3d07852bab2a92d8458f8f9e46
Neekon <neekon91@gmail.com>
2017-01-08 19:28:49 -0800

    last

--- src/underbar.js
+++ src/underbar.js
@@ -33,15 +33,15 @@
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
-  _.last = function(array, n) {
-  };
+  _.last = (array, n) => n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length - n));
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   //
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
-  _.each = function(collection, iterator) {
+  _.each = (collection, iterator) => {
+    
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
